51d99d725f3d18cefba7404c35807b8a48d3465e
SarahLJesus <sarahletjesus1908@gmail.com>
2024-03-13 11:34:57 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+    const items = ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Salvador', 'Fortaleza', 'Curitiba', 'Manaus', 'Recife', 'Porto Alegre', 'Brasília',].sort()
+</script>
+<template>
+  <h1>Lista de cidades ordenadas em ordem alfabética:</h1>
+  <ul>
+    <li v-for="(item, index) in items" :key="index"> {{ item }} </li>
+  </ul>
+</template>
